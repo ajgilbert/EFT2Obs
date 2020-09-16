12ad1ff9894b3a1c9a91ae02d12489fe9e6bc5bf
Andrew Gilbert <andrew.gilbert@cern.ch>
2020-09-16 16:30:49 +0200

    workaround for yoda trying to compile against root on lxplus

--- scripts/setup_rivet.sh
+++ scripts/setup_rivet.sh
@@ -6,4 +6,4 @@ source env.sh
 if [ -z "${RIVET_VERSION}" ]; then echo "ERROR: environment variable RIVET_VERSION is not set"; exit 1; fi
 
 wget "https://phab.hepforge.org/source/rivetbootstraphg/browse/${RIVET_VERSION}/rivet-bootstrap?view=raw" -O rivet-bootstrap
-bash rivet-bootstrap
+enable_root=no bash rivet-bootstrap
