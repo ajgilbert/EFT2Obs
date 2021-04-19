d62f555f6b66c6607b3bf516c56b1fc07a1c4377
Matthew Knight <matthewdknight9@gmail.com>
2021-04-19 18:45:39 +0200

    Added 'allboost' function to account for the possibility that the incoming particles have non-zero pt.

--- scripts/standalone_reweight.py
+++ scripts/standalone_reweight.py
@@ -7,6 +7,7 @@ import argparse
 import math
 import json
 import numpy
+import copy
 from collections import defaultdict
 
 
@@ -183,6 +184,68 @@ class StandaloneReweight:
                 out[3] = 0
             return out
 
+    def rotZ(self, p, angle):
+        p[1], p[2] = p[1]*math.cos(angle)-p[2]*math.sin(angle),  p[1]*math.sin(angle)+p[2]*math.cos(angle)
+        return p
+
+    def rotY(self, p, angle):
+        p[1], p[3] = p[1]*math.cos(angle)+p[3]*math.sin(angle),  -p[1]*math.sin(angle)+p[3]*math.cos(angle)
+        return p
+
+    def allboost(self, p, pboost):
+        """
+        Strategy here is to rotate pboost such that it lies along the z axis. Then boost pboost 
+        so that it is in its rest frame. Perform the same transformations to p, and then undo
+        the rotations at the end.
+        """
+        p, pboost = copy.copy(p), copy.copy(pboost) #force pass by value
+
+        #rotate around z axis such that there is no y component
+        if pboost[1]!=0:
+            z_rot_angle = math.atan(pboost[2]/pboost[1])
+        elif pboost[2]>0:
+            z_rot_angle = math.pi/2
+        else:
+            z_rot_angle = -math.pi/2
+
+        p = self.rotZ(p, -z_rot_angle)
+        pboost = self.rotZ(pboost, -z_rot_angle)
+
+        #rotate around y axis so pboost lies along z axis
+        r = math.sqrt(pboost[1]**2+pboost[2]**2+pboost[3]**2)
+        y_rot_angle = math.acos(pboost[3]/r)
+
+        p = self.rotY(p, -y_rot_angle)
+        pboost = self.rotY(pboost, -y_rot_angle)
+
+        #perform Lorentz boost
+        p = self.zboost(p, pboost)
+        pboost = self.zboost(pboost, pboost)
+
+        #undo rotations
+        p = self.rotY(p, y_rot_angle)
+        pboost = self.rotY(pboost, y_rot_angle)
+        p = self.rotZ(p, z_rot_angle)
+        pboost = self.rotZ(pboost, z_rot_angle)
+    
+        if abs(p[1]) < 1e-6 * p[0]:
+            p[1] = 0
+        if abs(p[2]) < 1e-6 * p[0]:
+            p[2] = 0
+        if abs(p[3]) < 1e-6 * p[0]:
+            p[3] = 0
+        if abs(pboost[1]) < 1e-6 * pboost[0]:
+            pboost[1] = 0
+        if abs(pboost[2]) < 1e-6 * pboost[0]:
+            pboost[2] = 0
+        if abs(pboost[3]) < 1e-6 * pboost[0]:
+            pboost[3] = 0
+
+        #check that transformations put pboost in rest frame
+        assert pboost[1]==pboost[2]==pboost[3]==0
+
+        return p
+
     def ComputeWeights(self, parts, pdgs, hels, stats, alphas, dohelicity=True, verb=False):
         assert len(parts) == len(pdgs) == len(hels) == len(stats)
         res = [1.0] * self.N
@@ -241,7 +304,11 @@ class StandaloneReweight:
             raise Exception("More than two initial particles.")
 
         for part in final_parts:
-            com_final_parts.append(self.zboost(part, pboost))
+            if (pboost[1]!=0) or (pboost[2]!=0): #if non-zero pt boost do boost in all directions
+                com_final_parts.append(self.allboost(part, pboost))
+            else: #otherwise just boost along z
+                com_final_parts.append(self.zboost(part, pboost))
+            
 
         final_parts_i = self.invert_momenta(com_final_parts)
 
