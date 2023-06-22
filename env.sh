841f07158e00eefec646e4834c7be758ba999473
Andrew Gilbert <andrew.gilbert@cern.ch>
2023-06-22 09:44:20 +0200

    Changes for MG_2_9_15 & CMSSW
     - env.sh will now check for `lhapdf-config` in the PATH, and use that instead of installing lhapdf locally
     - When making a LO gridpack, MG no longer generates any actual events, which we need in order to run and precompile the reweight module. To work around this we now generate some events manually.
     - Several old MG patches are no longer needed, but at least one patch needed for the reweight code
     - Not yet done: need to force that f2py3 is picked up

--- env.sh
+++ env.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
-export LHAPDF_CONFIG_PATH="${PWD}/lhapdf/bin/lhapdf-config"
-export PYTHONPATH="${PWD}/lhapdf/lib64/python2.7/site-packages:${PYTHONPATH}"
-export LD_LIBRARY_PATH="${PWD}/lhapdf/lib:${LD_LIBRARY_PATH}"
+
+# If we have a local lhapdf install, use it
+if command -v lhapdf-config &> /dev/null; then
+  export LHAPDF_CONFIG_PATH=$(command -v lhapdf-config)
+  export EFTOBS_LOCAL_LHAPDF=0
+else
+  export EFTOBS_LOCAL_LHAPDF=1
+  export LHAPDF_CONFIG_PATH="${PWD}/lhapdf/bin/lhapdf-config"
+  export PYTHONPATH="${PWD}/$(echo lhapdf/lib64/python*/site-packages):${PYTHONPATH}"
+  export LD_LIBRARY_PATH="${PWD}/lhapdf/lib:${LD_LIBRARY_PATH}"
+fi
+# or if we have some pre-installed lhapdf, use that instead
 export RIVET_ANALYSIS_PATH=${PWD}/RivetPlugins
-export MG_DIR="MG5_aMC_v2_6_7"
-export MG_TARBALL="MG5_aMC_v2.6.7.tar.gz"
+export MG_DIR="MG5_aMC_v2_9_15"
+export MG_TARBALL="MG5aMC_LTS_2.9.15.tgz"
 export RIVET_VERSION="3.0.1"
 export DEBUG_SCRIPTS=0
 
--- scripts/setup_mg5.sh
+++ scripts/setup_mg5.sh
@@ -8,24 +8,28 @@ if [ -z "${LHAPDF_CONFIG_PATH}" ]; then echo "ERROR: environment variable LHAPDF
 if [ -z "${MG_DIR}" ]; then echo "ERROR: environment variable MG_DIR is not set"; exit 1; fi
 if [ -z "${MG_TARBALL}" ]; then echo "ERROR: environment variable MG_TARBALL is not set"; exit 1; fi
 
-LHAPDF_VERSION="LHAPDF-6.2.1"
-wget "https://lhapdf.hepforge.org/downloads/?f=${LHAPDF_VERSION}.tar.gz" -O "${LHAPDF_VERSION}.tar.gz"
-tar xf "${LHAPDF_VERSION}.tar.gz"
-rm "${LHAPDF_VERSION}.tar.gz"
-mkdir lhapdf
-pushd "${LHAPDF_VERSION}"
-	./configure --prefix="${IWD}/lhapdf/"
-	make
-	make install
-popd
-rm -r "${LHAPDF_VERSION}"
 
+if [ ${EFTOBS_LOCAL_LHAPDF} -eq 1 ]; then
+  echo "ERROR: environment variable LHAPDF_CONFIG_PATH is not set" 
+  LHAPDF_VERSION="LHAPDF-6.5.3"
+  wget "https://lhapdf.hepforge.org/downloads/?f=${LHAPDF_VERSION}.tar.gz" -O "${LHAPDF_VERSION}.tar.gz"
+  tar xf "${LHAPDF_VERSION}.tar.gz"
+  rm "${LHAPDF_VERSION}.tar.gz"
+  mkdir lhapdf
+  pushd "${LHAPDF_VERSION}"
+	  PYTHON_VERSION=3 ./configure --prefix="${IWD}/lhapdf/"
+    make
+    make install
+  popd
+  rm -r "${LHAPDF_VERSION}"
+fi
+#exit 0
 
 if [ -d "${MG_DIR}" ]; then
   echo "Directory ${MG_DIR} already exists, remove this first to re-install"
   exit 1
 fi
-wget "https://launchpad.net/mg5amcnlo/2.0/2.6.x/+download/${MG_TARBALL}"
+wget "https://launchpad.net/mg5amcnlo/3.0/3.5.x/+download/${MG_TARBALL}"
 tar -zxf "${MG_TARBALL}"
 rm "${MG_TARBALL}"
 
@@ -40,23 +44,24 @@ rm "${MG_TARBALL}"
 
 pushd "${MG_DIR}"
 	./bin/mg5_aMC ../mgconfigscript
-  patch madgraph/core/helas_objects.py "${IWD}/setup/helas_objects.py.patch"
-  patch madgraph/interface/master_interface.py "${IWD}/setup/master_interface.py.patch"
+  patch madgraph/interface/reweight_interface.py "${IWD}/setup/reweight_interface.py.patch"
+  #patch madgraph/core/helas_objects.py "${IWD}/setup/helas_objects.py.patch"
+  #patch madgraph/interface/master_interface.py "${IWD}/setup/master_interface.py.patch"
 	pushd HEPTools/MG5aMC_PY8_interface
-		patch MG5aMC_PY8_interface.cc "${IWD}/setup/MG5aMC_PY8_interface.cc.patch"
-		patch MG5aMC_PY8_interface.cc "${IWD}/setup/corrector_MG5aMC_PY8_interface.cc.patch"
-		ln -s "${IWD}/setup/WeightCorrector.h" ./
-		python compile.py ../pythia8
+		#patch MG5aMC_PY8_interface.cc "${IWD}/setup/MG5aMC_PY8_interface.cc.patch"
+		#patch MG5aMC_PY8_interface.cc "${IWD}/setup/corrector_MG5aMC_PY8_interface.cc.patch"
+		#ln -s "${IWD}/setup/WeightCorrector.h" ./
+		#python compile.py ../pythia8
 	popd
 	pushd HEPTools/pythia8/include/Pythia8Plugins
-		ln -s "${IWD}/setup/WeightCorrector.h" ./
+		#ln -s "${IWD}/setup/WeightCorrector.h" ./
 	popd
 	pushd Template/NLO/MCatNLO
 		# Fix the order in which LD_LIBRARY_PATH is constructed, otherwise we will
 		# pick up the (incompatible) hepmc library from RIVET
-		patch shower_template.sh "${IWD}/setup/shower_template.sh.patch"
+		#patch shower_template.sh "${IWD}/setup/shower_template.sh.patch"
 		# Minimal changes to add the reweighting weights to the hepmc output and do
 		# the weight transformation
-		patch srcPythia8/Pythia82_hep.cc "${IWD}/setup/Pythia82_hep.cc.patch"
+		#patch srcPythia8/Pythia82_hep.cc "${IWD}/setup/Pythia82_hep.cc.patch"
 	popd
 popd
