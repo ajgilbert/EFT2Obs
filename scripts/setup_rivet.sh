1bb48989dcd74784d563e0f1dc7ab06c510a3f67
Andrew Gilbert <andrew.gilbert@cern.ch>
2024-02-19 15:28:27 +0100

    remove cmake3 flag

--- scripts/setup_rivet.sh
+++ scripts/setup_rivet.sh
@@ -6,4 +6,4 @@ source env.sh
 if [ -z "${RIVET_VERSION}" ]; then echo "ERROR: environment variable RIVET_VERSION is not set"; exit 1; fi
 
 wget "https://gitlab.com/hepcedar/rivetbootstrap/raw/${RIVET_VERSION}/rivet-bootstrap" -O rivet-bootstrap
-enable_root=no CMAKE=cmake3 PYTHON_EXE=python3 USE_VENV=0 HEPMC3=0 HEPMC_VERSION="2.06.11" bash rivet-bootstrap
+enable_root=no PYTHON_EXE=python3 USE_VENV=0 HEPMC3=0 HEPMC_VERSION="2.06.11" bash rivet-bootstrap
